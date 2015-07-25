3f4a8cee6cd2eb83fb25f203c39d92bdf691a572
ssc9309 <ssc9309@hotmail.com>
2015-07-24 23:58:18 -0400

    Initial commit

---
+++ TCPServerConsole/TCPServerConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPServerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+
+
+
+        }
+    }
+}
