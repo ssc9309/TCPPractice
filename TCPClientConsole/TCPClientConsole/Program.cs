ffd62e8272cacd6b49c285fe97e7ad5116d49dc1
ssc9309 <ssc9309@hotmail.com>
2015-07-25 00:45:03 -0400

    Hand shaking simple messages
    server:
    - need to time out of the client disconnected.
    - need to handle if the stream was zero.
    - i think this happens when stream.close on client side

---
+++ TCPClientConsole/TCPClientConsole/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace TCPClientConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program();
+
+            p.SendMessage();
+            
+
+        }
+
+        void SendMessage()
+        {
+            TcpClient client = new TcpClient();
+
+            client.Connect("192.168.1.15", 8888);
+
+            NetworkStream ns = client.GetStream();
+
+            Console.WriteLine("Enter message. q to quit");
+
+            
+            string input = "";
+            /*
+            while (input != "q")
+            {
+
+                input = Console.ReadLine();
+
+                byte[] bytes = System.Text.Encoding.ASCII.GetBytes(input + "Hank");
+
+                ns.Write(bytes, 0, bytes.Length);
+                ns.Flush();
+            }
+            */
+
+            for (int x= 0; x<1000; x++)
+            {
+                input = "I hate shakespeare. 16843215846351358463818438483768438463";
+                byte[] bytes = System.Text.Encoding.ASCII.GetBytes(input + "Hank");
+
+                ns.Write(bytes, 0, bytes.Length);
+                ns.Flush();
+
+                byte[] bytes2 = new byte[70000];
+
+                ns.Read(bytes2, 0, client.ReceiveBufferSize);
+
+                string msg = System.Text.Encoding.ASCII.GetString(bytes2);
+                msg = msg.Substring(0, msg.IndexOf("Hank"));
+
+                Console.WriteLine(msg);
+            }
+
+            ns.Close();
+            client.Close();
+            
+        }
+    }
+}
--- TCPServerConsole/TCPServerConsole/Program.cs
+++ TCPServerConsole/TCPServerConsole/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+
 
 namespace TCPServerConsole
 {
@@ -12,7 +16,61 @@ namespace TCPServerConsole
         {
             var p = new Program();
 
+            p.StartServer();
 
+        }
+
+        void StartServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 8888);
+            TcpClient client = new TcpClient();
+
+            listener.Start();
+
+            Console.WriteLine("Server started.");
+            //Console.WriteLine("IP: " + IPAddress.)
+
+            client = listener.AcceptTcpClient();
+
+            Console.WriteLine("Accepted TCP client");
+            int count = 0;
+
+            while (true)
+            {
+                try
+                {
+                    NetworkStream stream = client.GetStream();
+
+                    byte[] bytes = new byte[70025];
+                    //byte[] bytes;
+
+                    stream.Read(bytes, 0, (int)client.ReceiveBufferSize);
+
+                    if (bytes.Length == 0)
+                        break;
+
+                    string msg = System.Text.Encoding.ASCII.GetString(bytes);
+                    msg = msg.Substring(0, msg.IndexOf("Hank"));
+
+                    Console.WriteLine("Received: " + msg);
+
+                    bytes = System.Text.Encoding.ASCII.GetBytes("HSHank");
+
+                    stream.Write(bytes, 0, bytes.Length);
+                    count++;
+                }
+                catch(Exception exc)
+                {
+                    Console.WriteLine("Error: " + exc);
+                    Console.WriteLine("fuck it");
+                    break;
+                }
+            }
+
+            client.Close();
+            listener.Stop();
+            Console.WriteLine("The end " + count);
+            Console.ReadLine();
 
         }
     }
